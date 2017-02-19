8e6f594afd2f65bc007ca983dcb0767f8a5edad7
Manda <mandaniaina@gmail.com>
2017-02-19 14:17:59 -0500

    nunit+

---
+++ Anagrammes/Anagrammes.Test/ApplicationTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Anagrammes.Test
+{
+    [TestFixture]
+    class ApplicationTest
+    {
+        [Test]
+        public void test()
+        {
+            Assert.True(true);
+        }
+    }
+}
