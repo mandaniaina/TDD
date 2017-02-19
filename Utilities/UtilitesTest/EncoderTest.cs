482fedabb93609cb30281a27f45683b8058d294f
Manda <mandaniaina@gmail.com>
2017-02-19 13:56:48 -0500

    nouveaux tests

---
+++ Utilities/UtilitesTest/EncoderTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Utilities;
+
+namespace UtilitesTest
+{
+    [TestFixture]
+    public class EncoderTest
+    {
+        [Test]
+        public void StringToArayOfInt()
+        {
+            int[] verify = { 2, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2 };
+            string value = "aBabcd zyxz";
+            Encoder encoder = new Encoder();
+            int[] encodedValue = encoder.LetterToArrayOfInt(value);
+            Assert.AreEqual(verify, encodedValue);
+        }
+
+    }
+}
---
+++ Utilities/Utilities/Encoder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utilities
+{
+    class Encoder
+    {
+        public int[] LetterToArrayOfInt(string value)
+        {
+            int[] encodedValue = new int[26];
+            foreach (char c in value)
+            {
+                if (c >= 97 && c <= 122)
+                {
+                    encodedValue[c - 97]++;
+                }
+                if (c >= 65 && c <= 90)
+                {
+                    encodedValue[c - 65]++;
+                }
+            }
+            return encodedValue;
+        }
+    }
+}
